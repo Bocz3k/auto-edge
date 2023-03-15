a1e96a69545d1eba588805904ac42d4761317923
NieBoczek <106301320+Bocz3k@users.noreply.github.com>
2023-03-15 14:45:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from time import sleep
 import string
 import random
 import subprocess
+print("Running!\n")
 subprocess.run(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
+print("Starting to open Microsoft Edge")
 excluded = []
 letters = list(string.ascii_lowercase)
 sleep(2)
+print("Microsoft Edge opened")
 for _ in range(10):
     for _ in range(len(letters)):
         choice = random.choice(letters)
@@ -14,7 +17,9 @@ for _ in range(10):
             pyg.hotkey('ctrl', 'l')
             pyg.press(choice)
             pyg.press('enter')
+            print(f"Succesfully entered letter [{choice.upper()}]")
             excluded.append(choice)
             break
     sleep(1)
+print("Terminating Microsoft Edge")
 subprocess.call(['taskkill', '/IM', 'msedge.exe'])
