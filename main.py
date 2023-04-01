9ec088c8909a957a88218cb81384834991d78b20
NieBoczek <106301320+Bocz3k@users.noreply.github.com>
2023-04-01 19:06:55 +0200

    Update main.py
    needed more letters from alphabet ¯\_(ツ)_/¯

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ print("Please don't click during the process\n")
 subprocess.run(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
 print("Starting to open Microsoft Edge")
 excluded = []
+excluded2 = []
 letters = list(string.ascii_letters)
 sleep(0.5)
 print("Microsoft Edge opened")
-for _ in range(33):
+for _ in range(67):
     for _ in range(len(letters)):
         choice = random.choice(letters)
-        if choice not in excluded:
+        choice2 = random.choice(letters)
+        if choice not in excluded and choice2 not in excluded2:
             pyg.hotkey('ctrl', 'l')
             pyg.press(choice)
+            pyg.press(choice2)
             pyg.press('enter')
-            print(f"Succesfully entered letter [{choice.upper()}]")
+            print(f"Succesfully entered: {choice}{choice2}")
             excluded.append(choice)
+            excluded2.append(choice2)
+            sleep(0.1)
             break
-    sleep(0.2)
 print("Terminating Microsoft Edge")
 subprocess.call(['taskkill', '/IM', 'msedge.exe'])
